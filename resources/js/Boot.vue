84c83a06f8fe40354b0baaa2bef6a4cf64760d9e
Youness <sidkiyouness@gmail.com>
2022-04-13 11:27:32 +0000

    projet init

---
+++ resources/js/Boot.vue
@@ -0,0 +1,14 @@
+<template lang="">
+   <div>
+        <h1 class="text-5xl text-red-700 text-center">MASKA</h1>        
+    </div>
+</template>
+
+<script lang='ts'>
+import {defineComponent, ref} from 'vue';
+export default defineComponent({
+    setup() {
+        
+    }
+})
+</script>
---
+++ resources/js/app.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import Boot from './Boot.vue';
+
+const App = createApp({
+    components : {
+        Boot
+    }
+}).mount('#app');
